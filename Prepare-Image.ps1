8818a65e00ae7ac4af529e5a9e64d2940701b4e8
grec59 <agrocker19909@gmail.com>
2025-08-18 22:25:10 -0400

    Update Prepare-Image.ps1

--- Prepare-Image.ps1
+++ Prepare-Image.ps1
@@ -65,7 +65,7 @@ function Invoke-GroupPolicy {
         "SUCCESS: Computer Policy update completed. Check Event Viewer for details." | Out-File -FilePath $output -Encoding utf8 -Append
 }
     catch {
-        Write-Host "FAIL: Failed to update Computer Policy. Check Event Viewer for details. " -ForegroundColor Yellow
+        Write-Host "FAIL: Failed to update Computer Policy. Check Event Viewer for details." -ForegroundColor Yellow
         "FAIL: Unable to update Computer Policy." | Out-File -FilePath $output -Encoding utf8 -Append
         $($_.Exception.Message) | Out-File -FilePath $output -Encoding utf8 -Append
     }
