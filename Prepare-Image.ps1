a7b4a47ee190fa1d3fb9dc69edd159b1e4b97df3
grec59 <agrocker19909@gmail.com>
2025-08-30 17:55:41 -0400

    Remove Start-Sleep at script completion

--- Prepare-Image.ps1
+++ Prepare-Image.ps1
@@ -413,4 +413,3 @@ if ($sel.PowerConfig) {
 "Script execution complete." | Out-File -FilePath $output -Encoding utf8 -Append
 Write-Host "Script execution complete. See:"
 Write-Host "$output" -Foregroundcolor Gray
-Start-Sleep 2
